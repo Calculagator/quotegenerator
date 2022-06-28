e1c010f3d8be6a505fe01197377df958c865fd7d
Joel Amunrud <jda@5kextra.local>
2022-06-28 15:37:38 -0400

    more buttons

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,46 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
+import { ChevronDown,ChevronUp } from "../icons";
+import { removeItem,increase,decrease } from "../features/cart/cartSlice";
 
 const CartItem = (props) => {
-    return (
-        <article className='cart-item'>
-            <img src = {props.img} alt={props.title} />
-            <div>
-                <h4>{props.title}</h4>
-                <h4 className = 'item-price'>${props.price}</h4>
-                <button classname ='remove-btn'>remove</button>
-            </div>
-        </article>
+  const dispatch = useDispatch();
+  return (
+    <article className='cart-item'>
+      <img src = {props.img} alt={props.title} />
+      <div>
+        <h4>{props.title}</h4>
+        <h4 className = 'item-price'>${props.price}</h4>
+        <button 
+          className ='remove-btn' 
+            onClick={()=> {
+              dispatch(removeItem(props.id))
+            }}>
+            remove
+          </button>
+      </div>
+      <div>
+        <button 
+          className="amount-btn"
+          onClick={()=> {
+            dispatch(increase(props.id))
+          }}>
+          <ChevronUp />
+        </button>
+        <p className="amount">{props.amount}</p>
+        <button 
+          className="amount-btn"
+          onClick={()=> {
+            if (amount <=1) {
+              
+            }
+            dispatch(decrease(props.id))
+          }}>
+          <ChevronDown />
+          </button>
+      </div>
+    </article>
 
-    );
+  );
 };
 
 export default CartItem;
